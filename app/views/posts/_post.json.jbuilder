569abd1f5f8412874d5f254260b344bdef56a2e1
David9203 <davidrendon9203@gmail.com>
2018-04-15 18:09:50 -0500

    adddeviseboostrapandpapercliptouserspict

--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,4 @@
 json.extract! post, :id, :band, :album, :genere, :comments, :created_at, :updated_at
 json.url post_url(post, format: :json)
+
+
---
+++ db/migrate/20180415200446_add_avatar_to_posts.rb
@@ -0,0 +1,9 @@
+class AddAvatarToPosts < ActiveRecord::Migration[5.1]
+  def up
+    add_attachment :posts, :avatar
+  end
+ 
+  def down
+    remove_attachment :posts, :avatar
+  end
+end
